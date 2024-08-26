88850b7928f852b6671b6e857f90bc214eb9e142
Rinat <rinat@hey.com>
2024-08-26 13:57:01 +0300

    feat: homepage without siwe (#51)

--- packages/nextjs/middleware.ts
+++ packages/nextjs/middleware.ts
@@ -7,8 +7,8 @@ const SIGN_IN_PAGE = "/siwe";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
 
-  // If no token and the request is not for the sign-in page, redirect to the sign-in page
-  if (!token && request.nextUrl.pathname !== SIGN_IN_PAGE) {
+  // If no token and the request is not for the sign-in or home page, redirect to the sign-in page
+  if (!token && request.nextUrl.pathname !== "/" && request.nextUrl.pathname !== SIGN_IN_PAGE) {
     const url = request.nextUrl.clone();
     url.pathname = SIGN_IN_PAGE;
     return NextResponse.redirect(url);

e54b6bf43ad9d9f763766b3779dc211837a237f8
Shiv Bhonde <shivbhonde04@gmail.com>
2024-11-10 15:52:47 +0700

    remove grantees section when there no grantees

--- packages/nextjs/app/_components/Grants/index.tsx
+++ packages/nextjs/app/_components/Grants/index.tsx
@@ -8,6 +8,8 @@ export const ApprovedGrants = async () => {
     grant.stages.some(stage => stage.status === "approved" || stage.status === "completed"),
   ) as NonNullable<GrantWithStages>[];
 
+  if (approvedGrants.length === 0) return null;
+
   return (
     <div className="py-10 px-4 flex flex-col items-center w-full">
       <h2 className="text-3xl font-black !mb-0">Grantees</h2>

90007ab97f39894a72a98ac491d67d44d22520dc
Damian <damianmarti@gmail.com>
2025-04-15 16:47:42 -0300

    Rename approvalVote to rejectVote on createLargeRejectVote

--- packages/nextjs/services/database/repositories/large-reject-votes.ts
+++ packages/nextjs/services/database/repositories/large-reject-votes.ts
@@ -5,6 +5,6 @@ import { largeRejectVotes } from "~~/services/database/config/schema";
 export type LargeRejectVoteInsert = InferInsertModel<typeof largeRejectVotes>;
 export type LargeRejectVote = InferSelectModel<typeof largeRejectVotes>;
 
-export async function createLargeRejectVote(approvalVote: LargeRejectVoteInsert) {
-  return await db.insert(largeRejectVotes).values(approvalVote).returning({ id: largeRejectVotes.id });
+export async function createLargeRejectVote(rejectVote: LargeRejectVoteInsert) {
+  return await db.insert(largeRejectVotes).values(rejectVote).returning({ id: largeRejectVotes.id });
 }
